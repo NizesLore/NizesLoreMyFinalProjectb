7216ac2bf25d56a20b5c9b5763bc977c3ee5b6b8
NizesLore <fenciszn@hotmail.com>
2021-02-13 14:44:18 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,38 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {   // CONSTRUCTOR INJECTION
+       
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            // iş kodları,if ler var
+            // Yetkisi var mı? Evetse DATA ACCESS e diyor ki bana ürünleri verebilirsin
+            return _productDal.GetAll();
+        }
+
+        public List<Product> GetAllByCategoryId(int id)
+        {
+            return _productDal.GetAll(p=> p.CategoryId==id);
+        }
+
+        public List<Product> GetByUnitPrice(decimal min, decimal max)
+        {
+            return _productDal.GetAll(p=> p.UnitPrice>=min && p.UnitPrice<=max );
+
+        }
+    }
+}
